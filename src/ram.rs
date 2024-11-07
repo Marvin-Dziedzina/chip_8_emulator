9ca1c89e71517ede8b1cead1588e02f74ed93f6a
Mixaffected <marvin.dziedzina@gmail.com>
2024-11-07 23:50:15 +0100

    Fixed counting issue

--- src/ram.rs
+++ src/ram.rs
@@ -76,7 +76,7 @@ impl Stack {
 
     /// Pushes data onto the stack.
     pub fn push(&mut self, data: u16) -> Result<(), MemoryError> {
-        if self.stack_pointer as usize >= self.stack.len() {
+        if self.stack_pointer as usize >= self.stack.len() - 1 {
             return Err(MemoryError::StackOverflow);
         };
 
